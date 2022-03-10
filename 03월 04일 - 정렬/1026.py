a7b4a3f3d081f38c65107a7e0d38c81fbf0c6f51
Hong-Jinseo <hong_jinseo@naver.com>
2022-03-10 16:20:18 +0900

    [정렬] 3월 10일 - Update

--- 03월 04일 - 정렬/1026.py
+++ 03월 04일 - 정렬/1026.py
@@ -7,9 +7,10 @@ b_list = list(map(int, input().split()))
 
 s = 0
 
-for _ in range(N):
-    s += max(a_list) * min(b_list)  # a의 최댓값 * b의 최솟값
-    a_list.remove(max(a_list))      # a에서 최댓값 삭제
-    b_list.remove(min(b_list))      # b에서 최솟값 삭제
+a_list.sort()
+b_list.sort(reverse=True)
 
+for i in range(N):
+    s += a_list[i] * b_list[i]
+    
 print(s)
